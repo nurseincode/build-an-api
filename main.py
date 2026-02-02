dc788982d85f98c5a49a88e5bc57eb9537296560
https://github.com/nurseincode <nurseincode@gmail.com>
2026-02-02 22:59:17 +1100

    Created connection string

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from init import db, ma
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psy'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://lms_dev:123456@localhost5432/lms_db'
