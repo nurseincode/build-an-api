63ab83e07fb4c3acd0f71941be59e82b6f6b403b
https://github.com/nurseincode <nurseincode@gmail.com>
2026-03-06 23:32:03 +1100

    Created init and seed cmds

--- blueprints/cli_bp.py
+++ blueprints/cli_bp.py
@@ -1,3 +1,13 @@
 from flask import Blueprint
+from init import db
 
 cli_bp = Blueprint('cli_commands', __name__)
+
+@cli_bp.cli.command('init')
+def create_tables():
+    db.drop_all()
+    db.create_all()
+    print('Tables created')
+
+@cli_bp.cli.comman('seed')
+def seed_tables():
