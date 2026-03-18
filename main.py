eb905027e011931cbf5c4413b55bbfe6e735bac7
https://github.com/nurseincode <nurseincode@gmail.com>
2026-03-18 22:13:53 +1100

    Registered students_bp

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from init import db, ma 
 import os
 from blueprints.db_bp import db_bp
+from blueprints.students_bp import students_bp
 
 def create_app():
     app = Flask(__name__)
@@ -13,5 +14,6 @@ def create_app():
     ma.init_app(app)
 
     app.register_blueprint(db_bp)
+    app.register_blueprint(students_bp)
 
     return app
